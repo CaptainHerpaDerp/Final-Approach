4fb0db54a318d9842a8141b636a98ae08bdfc1f0
Ruben Smith <ruben.smith@ebuynow.com>
2022-04-25 17:59:46 +0200

    Player Class
    Player jumping and friction physics

---
+++ FinalApproach/GXPEngine/Player.cs
@@ -0,0 +1,96 @@
+﻿using GXPEngine;
+using System;
+
+public class Player : AnimationSprite
+{
+    public bool up, left, right, hasJumped;
+
+    public Vec2 previousVelocity;
+
+    public int directionX, directionY;
+
+    protected Vec2 position;
+
+    protected Vec2 velocity;
+
+    public float speed;
+
+
+    public Player() : base("barry.png", 7, 1)
+    {
+        hasJumped = true;
+        currentFrame = 1;
+       
+    }
+
+
+    void UpdateScreenPosition()
+    {
+        x = position.x;
+        y = position.y;
+    }
+
+
+    public void Update()
+    {
+
+        UpdateScreenPosition();
+
+        up = Input.GetKey(Key.W);
+        left = Input.GetKey(Key.A);
+        right = Input.GetKey(Key.D);
+
+        if (left)
+        {
+            velocity.x = -3;
+        }
+
+        else if (right)
+        {
+            velocity.x = 3;
+        }
+
+        else
+        {
+            velocity.x = 0;
+        }
+
+        if (up && !hasJumped)
+        {
+            position.y -= 6.5f;
+            velocity.y = -40f;
+            hasJumped = true;
+        }
+
+        if (hasJumped)
+        {
+            velocity.y += 0.50f;
+        }
+
+        if (position.y > 500)
+        {
+            position.y = 500;
+            hasJumped = false;
+        }
+
+
+        if (!hasJumped)
+        {
+            velocity.y = 0;
+        }
+
+        var percentCurrent = velocity * 0.1f;
+
+        var percentPrevious = previousVelocity * 0.9f;
+
+        velocity = percentCurrent + percentPrevious;
+    
+
+        position += velocity;
+
+        previousVelocity = velocity;
+
+
+    }
+
+}
---
+++ FinalApproach/GXPEngine/vec2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using GXPEngine; // Allows using Mathf functions
+
+public struct Vec2
+{
+    public float x;
+    public float y;
+
+    public Vec2(float pX = 0, float pY = 0)
+    {
+        x = pX;
+        y = pY;
+    }
+
+    // TODO: Implement Length, Normalize, Normalized, SetXY methods (see Assignment 1)
+
+    // TODO: Implement subtract, scale operators
+
+    public static Vec2 operator +(Vec2 left, Vec2 right)
+    {
+        return new Vec2(left.x + right.x, left.y + right.y);
+    }
+
+    public static Vec2 operator -(Vec2 left, Vec2 right)
+    {
+        return new Vec2(left.x - right.x, left.y - right.y);
+    }
+    public static Vec2 operator *(Vec2 left, Vec2 right)
+    {
+        return new Vec2(left.x * right.x, left.y * right.y);
+    }
+
+    public static Vec2 operator *(Vec2 left, float right)
+    {
+        return new Vec2(left.x * right, left.y * right);
+    }
+
+
+    public override string ToString()
+    {
+        return $"({x},{y})";
+    }
+
+    public float Length()
+    {
+        return Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2));
+    }
+
+    public void Normalize()
+    {
+        var length = Length();
+
+        if (length == 0) return;
+        x = x / length;
+        y = y / length;
+
+    }
+
+    public Vec2 Normalized()
+    {
+        var length = Length();
+
+        if (length != 0)
+        {
+            return new Vec2(x / length, y / length);
+        }
+
+        return new Vec2(0, 0);
+
+    }
+
+    public void SetXy(float x, float y)
+    {
+        this.x = x; this.y = y;
+    }
+
+    public void SetXy(Vec2 vec)
+    {
+        SetXy(vec.x, vec.y);
+    }
+
+    public void SetLength(float length)
+    {
+        Normalize();
+
+        x *= length;
+        y *= length;
+    }
+
+}
